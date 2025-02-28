3ffc9459cae5ed376d0206b52c1bb9d67b428d55
pioz <epilotto@gmx.com>
2025-02-28 14:08:35 +0100

    Add dir_config for windows ZBar

--- ext/qrscan/extconf.rb
+++ ext/qrscan/extconf.rb
@@ -4,6 +4,8 @@
 require "mkmf"
 
 dir_config("qrscan", "/opt/homebrew/include", "/opt/homebrew/lib")
+dir_config("qrscan", "C:/Program Files/ZBar/include", "C:/Program Files/ZBar/lib")
+dir_config("qrscan", "C:/Program Files (x86)/ZBar/include", "C:/Program Files (x86)/ZBar/lib")
 
 # Verifica la presenza dell'header e della libreria (opzionale)
 have_header("zbar.h") or abort "zbar.h not found"

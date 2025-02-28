6d1e510ef4dbb8ea1e3fdfbc9b7cdd57c1ecc96e
pioz <epilotto@gmx.com>
2025-02-28 15:02:56 +0100

    Improve extconf for windows compatibility

--- ext/qrscan/extconf.rb
+++ ext/qrscan/extconf.rb
@@ -2,10 +2,14 @@
 # https://guides.rubygems.org/gems-with-extensions/
 
 require "mkmf"
+require "rbconfig"
+host_os = RbConfig::CONFIG["host_os"]
 
-dir_config("qrscan", "/opt/homebrew/include", "/opt/homebrew/lib")
-dir_config("qrscan", "C:/Program Files/ZBar/include", "C:/Program Files/ZBar/lib")
-dir_config("qrscan", "C:/Program Files (x86)/ZBar/include", "C:/Program Files (x86)/ZBar/lib")
+dir_config("qrscan", "/opt/homebrew/include", "/opt/homebrew/lib") if host_os.include?("darwin")
+if host_os.match?(/mswin|mingw|cygwin/)
+  dir_config("qrscan", "C:/Program Files/ZBar/include", "C:/Program Files/ZBar/lib")
+  dir_config("qrscan", "C:/Program Files (x86)/ZBar/include", "C:/Program Files (x86)/ZBar/lib")
+end
 
 # Verifica la presenza dell'header e della libreria (opzionale)
 have_header("zbar.h") or abort "zbar.h not found"
